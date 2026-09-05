c34ff91a6af5202ebc31979ab288e09eb12c66e3
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-09-05 12:34:50 -0400

    many changes 3

--- marc-houle-tech/src/app/layout.tsx
+++ marc-houle-tech/src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import SiteChrome from "@/components/layout/SiteChrome";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
